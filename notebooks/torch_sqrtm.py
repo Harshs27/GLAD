cf57842c4336c4ac2a091517b0b526607012b13b
Harsh Shrivastava <harshshrivastava111@gmail.com>
2020-02-15 13:00:04 -0500

    Matrix square root and gradient calculated using torch

--- notebooks/torch_sqrtm.py
+++ notebooks/torch_sqrtm.py
@@ -2,44 +2,45 @@ import torch
 from torch.autograd import Variable
 from torch.autograd import Function
 import numpy as np
-import scipy.linalg
+#import scipy.linalg
 
 class MatrixSquareRoot(Function):
     """Square root of a positive definite matrix.
-
     NOTE: matrix square root is not differentiable for matrices with
           zero eigenvalues.
-    """
+    """    
     @staticmethod
     def forward(ctx, input):
-        #m = input.numpy().astype(np.float_)
-        m = input.detach().cpu().numpy().astype(np.float_)
-        sqrtm = torch.from_numpy(scipy.linalg.sqrtm(m).real)#.type_as(input)
-        ctx.save_for_backward(sqrtm) # save in cpu
-        sqrtm = sqrtm.type_as(input)
+        dim = input.shape[0]
+        norm = torch.norm(input)#.double())
+        #Y = input.double()/norm
+        Y = input/norm
+        I = torch.eye(dim,dim,device=input.device)#.double()
+        Z = torch.eye(dim,dim,device=input.device)#.double()
+        for i in range(20):
+            T = 0.5*(3.0*I - Z.mm(Y))
+            Y = Y.mm(T)
+            Z = T.mm(Z)
+        sqrtm = Y*torch.sqrt(norm)
+        ctx.mark_dirty(Y,I,Z)
+        ctx.save_for_backward(sqrtm)
         return sqrtm
 
     @staticmethod
     def backward(ctx, grad_output):
         grad_input = None
-        if ctx.needs_input_grad[0]:
-            #sqrtm, = ctx.saved_variables
-            sqrtm, = ctx.saved_tensors
-            #sqrtm = sqrtm.data.numpy().astype(np.float_)
-            sqrtm = sqrtm.data.numpy().astype(np.float_)
-            #gm = grad_output.data.numpy().astype(np.float_)
-            gm = grad_output.data.cpu().numpy().astype(np.float_)
-#            gm = np.eye(grad_output.shape[-1])
-            # Given a positive semi-definite matrix X,
-            # since X = X^{1/2}X^{1/2}, we can compute the gradient of the
-            # matrix square root dX^{1/2} by solving the Sylvester equation:
-            # dX = (d(X^{1/2})X^{1/2} + X^{1/2}(dX^{1/2}).
-            grad_sqrtm = scipy.linalg.solve_sylvester(sqrtm, sqrtm, gm)
-
-            grad_input = torch.from_numpy(grad_sqrtm).type_as(grad_output.data)
-        return Variable(grad_input)
-
-
+        sqrtm, = ctx.saved_tensors
+        dim = sqrtm.shape[0]
+        norm = torch.norm(sqrtm)
+        A = sqrtm/norm
+        I = torch.eye(dim, dim, device=sqrtm.device)#.double()
+        #Q = grad_output.double()/norm
+        Q = grad_output/norm
+        for i in range(20):
+            Q = 0.5*(Q.mm(3.0*I-A.mm(A))-A.t().mm(A.t().mm(Q)-Q.mm(A)))
+            A = 0.5*A.mm(3.0*I-A.mm(A))
+        grad_input = 0.5*Q
+        return grad_input    
 sqrtm = MatrixSquareRoot.apply
 
 
